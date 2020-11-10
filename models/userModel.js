15ef7e1eee5dc9b0976b1253ca66ffb3f84f628a
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2020-11-10 20:55:29 +0530

    implement auth, google auth using passport

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    googleId: String,
+    password: String,
+    name: String,
+    profilePic: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+
+module.exports = User;
---
+++ routes/auth/login.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+
+const User = require('../../models/userModel');
+
+router.get("/", (req, res) => {
+    res.send("Welcome to Login Page");
+});
+
+router.post("/", (req,res) => {
+
+    console.log(req.body);
+    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, (err) => {
+
+        if (err) {
+            console.log(err);
+          } else {
+            passport.authenticate("local")(req, res, function(){
+            res.send("Logged in sucessfully bud !");
+          });
+        }
+      });
+
+})
+module.exports = router;
