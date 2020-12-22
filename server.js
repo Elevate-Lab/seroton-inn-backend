da96b7d75a0fcf3be740b92b424db9685f38dee7
Elemento24 <mittalvishesh021@gmail.com>
2020-12-22 21:50:31 +0530

    Make the routes for getting all the Posts & a single post

--- server.js
+++ server.js
@@ -11,33 +11,33 @@ const User = require('./models/userModel');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* -------- Set up session ------------*/
 app.use(session({
-    secret: "Our little secret.",
-    resave: false,
-    saveUninitialized: false
-  }));
-  
-  app.use(passport.initialize());
-  app.use(passport.session());
-  /* -------- Session set up ended ------------*/
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+/* -------- Session set up ended ------------*/
 
 
-  /* -------- Passport serialise ------------*/
-  passport.use(User.createStrategy());
+/* -------- Passport serialise ------------*/
+passport.use(User.createStrategy());
 
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
   });
-  
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
-  });
-  /* -------- Passport serialise ended ------------*/
+});
+/* -------- Passport serialise ended ------------*/
 
 
 /*Routes Config*/
@@ -46,18 +46,20 @@ const indexRoute = require("./routes/index");
 const registerRoute = require('./routes/auth/register');
 const loginRoute = require('./routes/auth/login');
 const googleAuth = require('./routes/auth/googleAuth');
-const userOperation = require('./routes/user/user_operations')
+const userOperation = require('./routes/user/user_operations');
+const postsRoute = require('./routes/post');
 
 /*-----Routes Config End------*/
 
 
 /*App Config*/
 
-app.use("/",indexRoute);
+app.use("/", indexRoute);
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 app.use("/auth/google", googleAuth);
-app.use("/user" , userOperation);
+app.use("/user", userOperation);
+app.use("/posts", postsRoute);
 
 /*------App Config End--------*/
 
@@ -66,17 +68,17 @@ app.use("/user" , userOperation);
 const uri = process.env.MONGO_URL || "mongodb://localhost:27017/sereton-inn";
 
 mongoose.connect(uri, {
-    useNewUrlParser: true, 
-    useCreateIndex: true, 
-    useUnifiedTopology: true 
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully");
 });
 
 /*----Mongoose config End-----*/
 
-app.listen(port,() => {
-    console.log(`Server started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
