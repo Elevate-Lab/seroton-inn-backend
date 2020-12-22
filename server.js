cb945a8c26380ddf5f3bf9c5671238de202153e7
Adarsh Tripath <tripathiadarsh39@gmail.com>
2020-12-22 09:17:25 +0530

    modified user profile endpoints

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const User = require('./models/userModel');
 
-// require("dotenv/config");
+require("dotenv/config");
+
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 
 /* -------- Set up session ------------*/
